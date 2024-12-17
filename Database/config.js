23c5b20c30b3248bce9e5e7e7b0e4d7fcdd4dcd2
Ace Michel Mwashuma <acemichelnk@gmail.com>
2024-12-17 09:58:18 +0300

    Create config.js

---
+++ Database/config.js
@@ -0,0 +1,21 @@
+const knex = require('knex');
+require('dotenv').config();
+
+const db = knex({
+  client: 'pg', // Use 'mysql' for MySQL, 'sqlite3' for SQLite
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER || 'your_username',
+    password: process.env.DB_PASSWORD || 'your_password',
+    database: process.env.DB_NAME_SANAA || 'UNBNKD_wallet',
+  },
+  migrations: {
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
+  },
+});
+
+module.exports = db;
